aea38d19c631788b49362362973abc0d1082130d
laimison <laimison1@gmail.com>
2019-01-27 22:54:20 +0000

    added relay script

---
+++ relay.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import RPi.GPIO as GPIO
+import time
+import sys
+
+GPIO.setmode(GPIO.BCM)
+
+# GPIO.setmode(GPIO.BOARD)
+
+pinList = [2, 3, 4, 17, 27, 22, 10, 9]
+
+mypin = int(sys.argv[1])
+
+GPIO.setup(mypin,GPIO.OUT)
+GPIO.output(mypin, False)
+time.sleep(1);
+GPIO.output(mypin, True)
+GPIO.cleanup()
